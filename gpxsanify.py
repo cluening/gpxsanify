5d623c7e75a0bb95a8b78af11de6139b674a8044
Cory Lueninghoener <cluening@gmail.com>
2014-10-26 20:32:06 -0600

    Able to calculate distance and time differences

--- gpxsanify.py
+++ gpxsanify.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import xml.etree.ElementTree as elementtree
+import math, time
 
+namespace = "http://www.topografix.com/GPX/1/1"
 lastlat = 500
 lastlon = 500
+lasttimestruct = 0
 
 def main():
   inputfile = '/Users/cluening/GPS/GPX/Archive/2014-10-17 09.32.04 Day.gpx'
@@ -14,23 +17,42 @@ def main():
   doelement(tree.getroot())
 
 
-
+##
+# doelement()
+##
 def doelement(element):
   global lastlat
   global lastlon
+  global lasttimestruct
 
   if element.tag.endswith("trk"):
     print "Foo!"
     lastlat = 500
     lastlon = 500
+    lasttimestruct = 0
 
   if element.tag.endswith("trkpt"):
     print element.attrib
     print lastlat
+
+    timeelement = element.find("{%s}time" % namespace)
+    timestruct = time.strptime(timeelement.text, "%Y-%d-%mT%H:%M:%SZ")
+    lat = float(element.attrib['lat'])
+    lon = float(element.attrib['lon'])
+ 
     if(lastlat < 500 and lastlon < 500):
       print "Have a comparison!"
-    lastlat = float(element.attrib['lat'])
-    lastlon = float(element.attrib['lon'])
+      timediff = time.mktime(timestruct) - time.mktime(lasttimestruct)
+      distdiff = haversine(lon, lat, lastlon, lastlat)
+      print("Time difference: %f seconds" % timediff)
+      print("Dist difference: %f kilometers" % distdiff)
+      
+    lastlat = lat
+    lastlon = lon
+    lasttimestruct = timestruct
+
+#  timestruct1 = time.strptime(timestamp1, "%Y-%d-%mT%H:%M:%SZ")
+#  time.mktime(timestruct2) - time.mktime(timestruct1)
 
 #  print element.tag
 #  print element.attrib
@@ -38,6 +60,27 @@ def doelement(element):
 
   for child in element:
     doelement(child)
+
+##
+##  haversine() function.  Stolen from stacktrace
+##
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
+
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    return km 
 
 if __name__ == "__main__":
   main()
