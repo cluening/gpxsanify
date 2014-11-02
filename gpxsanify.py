b73801e324cde366b707d150b86e26b9c8f89ab9
Cory Lueninghoener <cluening@gmail.com>
2014-11-01 23:44:03 -0600

    Added some command line option support and the ability to autodetect namespace

--- gpxsanify.py
+++ gpxsanify.py
@@ -2,40 +2,105 @@
 
 import xml.etree.ElementTree as elementtree
 import math, time, sys, os
+import argparse
 
-namespace = "http://www.topografix.com/GPX/1/1"
+# Command line options to include:
+# - inputdir
+# - outputdir
+# - absurd speed (default to speed of sound)
+# - single file
+
+namespaces = ["http://www.topografix.com/GPX/1/0",
+              "http://www.topografix.com/GPX/1/1"]
 lastlat = 500
 lastlon = 500
 lasttimestruct = None
 lasttrkpt = None
 
 def main():
-  inputdir = "/Users/cluening/GPS/GPX/Archive"
-  outputdir = inputdir + ".sanified"
-  inputfile = '/Users/cluening/GPS/bayobench.gpx'
-  inputfile = '/Users/cluening/GPS/gpx.etrex/20100713.gpx'
-  inputfile = '2014-09-17 17.36.06 Day.gpx'
+  #inputdir = "/Users/cluening/GPS/GPX/Archive"
+  #outputdir = inputdir + ".sanified"
+  #inputfile = '/Users/cluening/GPS/bayobench.gpx'
+  #inputfile = '/Users/cluening/GPS/gpx.etrex/20100713.gpx'
+  #inputfile = '2014-09-17 17.36.06 Day.gpx'
 
-  try:
-    os.mkdir(outputdir)
-  except OSError:
-    print "Directory already exists"
+  # Set up the option parser
+  parser = argparse.ArgumentParser(description='Sanify those GPX files!')
+  parser.add_argument('--inputfile', dest='inputfile', action='store', 
+                      help="Single GPX file to sanify")
+  parser.add_argument('--outputfile', dest='outputfile', action='store', 
+                      help="Output file for single file sanification")
+  parser.add_argument('--inputdir', dest='inputdir', action='store', 
+                      help="Input directory to search for GPX files")
+  parser.add_argument('--outputdir', dest='outputdir', action='store', 
+                      help="Output directory for sanified GPX files")
+  args = parser.parse_args()
 
-#  elementtree.register_namespace('', "http://www.topografix.com/GPX/1/0")
-  elementtree.register_namespace('', "http://www.topografix.com/GPX/1/1")
+  # Verify the option input
+  if args.inputfile == None and args.inputdir == None:
+    print("Need an input file or input directory")
+    sys.exit(1)
+  if args.inputfile != None and args.inputdir != None:
+    print("Can't do both a single file and a directory at the same time")
+    sys.exit(1)
+  if args.inputfile != None and args.outputfile == None:
+    print("Need to specify an output file with an input file")
+    sys.exit(1)
+  if args.inputdir != None and args.outputdir == None:
+    print("Need to specify an output directory with an input directory")
+    sys.exit(1)
 
-#  for inputfile in [inputfile]:
-  for inputfile in os.listdir(inputdir):
+  if args.outputdir != None:
+    try:
+      os.mkdir(args.outputdir)
+    except OSError:
+      print "Directory already exists"
+  else:
+    # We didn't get an output directory, so we must be doing a single
+    # file with a full path
+    outputdir = ""
+
+
+  if args.inputfile != None:
+    inputfiles = [args.inputfile]
+  else:
+    try:
+      inputfiles = os.listdir(args.inputdir)
+    except OSError as detail:
+      print "Error opening %s: %s" % (args.inputdir, detail)
+      sys.exit(1)
+
+  for inputfile in inputfiles:
     if inputfile.endswith(".gpx"):
       print(inputfile)
-      tree = elementtree.parse(inputdir + "/" + inputfile)
-      doelement(tree.getroot(), None)
-      tree.write(outputdir + "/" + inputfile)
+      try:
+        tree = elementtree.parse(os.path.join(args.inputdir, inputfile))
+      except elementtree.ParseError as detail:
+        print("Bad file: %s: %s" % (inputfile, detail))
+        continue
+#      tree.write(sys.stdout)
+
+      for namespace in namespaces:
+        elementtree.register_namespace('', namespace)
+        trkpts = tree.getroot().findall("{%s}trk" % namespace)
+        if len(trkpts) > 0:
+          #print("Good namespace: %s" % namespace)
+          break
+      else:
+        # FIXME: should be a warning
+        print("Couldn't find any valid namespaces")
+        continue
+
+      doelement(tree.getroot(), None, namespace)
+      if args.outputfile != None:
+        tree.write(args.outputfile)
+      else:
+        tree.write(os.path.join(args.outputdir, inputfile))
 
 ##
 # doelement()
 ##
-def doelement(element, parent):
+def doelement(element, parent, namespace):
   global lastlat
   global lastlon
   global lasttimestruct
@@ -69,9 +134,7 @@ def doelement(element, parent):
       if distdiff/timediff > .067:
         print("Absurd speed! %f km/s" % (distdiff/timediff))
         print("%f, %f" % (lat, lon))
-        print parent
         parent.remove(lasttrkpt)
-        print("Removed")
       
     lastlat = lat
     lastlon = lon
@@ -86,10 +149,10 @@ def doelement(element, parent):
 #  print element.text
 
   for child in element:
-    doelement(child, element)
+    doelement(child, element, namespace)
 
 ##
-##  haversine() function.  Stolen from stacktrace
+##  haversine() function.  Stolen from stackoverflow
 ##
 def haversine(lon1, lat1, lon2, lat2):
     """
